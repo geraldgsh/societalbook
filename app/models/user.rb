70cb6003c8bae5ab6cdafaac722495006ca48562
geraldgsh <geraldgsh@hotmail.com>
2019-12-05 05:32:50 +0800

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
 end
