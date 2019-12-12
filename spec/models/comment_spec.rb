548243e18100c25b861440432a7cfa5ddc4429f4
J. Christian Romero <jcromerohdz@gmail.com>
2019-12-12 11:41:26 -0800

    Perfoming comments tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Content' do
+    before :each do
+      @user = User.create(name: 'author', email: 'author@email.com',
+                             password: '123465')
+      @post = @user.microposts.create(content: 'New post')
+      @comment = @post.comments.create(replay: 'this a comment',
+                                       user_id: @user.id)
+      expect(@comment.valid?).to eql(true)
+    end
+
+    it 'should be not be empty' do
+      @comment.replay = ''
+      expect(@comment.valid?).to eql(false)
+    end
+
+    it 'should not exceed 255 characters' do
+      @comment.replay = 'A' * 256
+      expect(@comment.valid?).to eql(false)
+    end
+
+    it 'should belong to a user' do
+      @comment.user = nil
+      expect(@comment.valid?).to eql(false)
+    end
+
+    it 'should belong to a micropost' do
+      @comment.micropost = nil
+      expect(@comment.valid?).to eql(false)
+    end
+  end
+
+
 end
