ac42151ee4e58d4b7d6cb6dc2f3e35cdc734d2f1
J. Christian Romero <jcromerohdz@gmail.com>
2019-12-12 11:33:06 -0800

    Performing likes rspec test

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(name: 'test', email: 'test@test.com',
+                      password: '123456')
+    @post = @user.microposts.create(content: 'test post')
+  end
+
+  it 'should have micropost_id' do
+   @like = @user.likes.build
+   @like.valid?
+   expect(@like.errors[:micropost]).to include('must exist')
+
+   @like = @user.likes.build(micropost_id: @post.id)
+   expect(@like.valid?).to eql(true)
+ end
+
+ it 'should have user_id' do
+  @like = @post.likes.build
+  @like.valid?
+  expect(@like.errors[:user]).to include('must exist')
+
+  @like = @post.likes.build(user_id: @user.id)
+  expect(@like.valid?).to eql(true)
+  end
+
+  it 'cant be on same post twice from same user' do
+    @like = @post.likes.create(user_id: @user.id)
+
+    @like2 = @post.likes.build(user_id: @user.id)
+    expect(@like2.valid?).to eql(false)
+    expect(@like2.errors[:user_id]).to include('has already been taken')
+  end
 end
