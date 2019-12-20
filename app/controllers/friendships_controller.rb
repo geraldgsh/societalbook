bb78c23262ac1e5abf465e778b1ac32303899db6
J. Christian Romero <jcromerohdz@gmail.com>
2019-12-20 07:33:48 -0800

    Improve friends list in the UI

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,7 +4,7 @@ class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.friendships.build(friend_id: params[:requestee_user_id], status: false)
     if @friendship.save
-      redirect_to users_friends_path(current_user), notice: 'Friend request created'
+      redirect_to users_friends_path(current_user), notice: 'Friend request sent'
     else
       redirect_to users_friends_path(current_user), alert: 'Something went wrong'
     end
@@ -14,10 +14,10 @@ class FriendshipsController < ApplicationController
     if params[:requestee_user_id]
       requestee =  User.find(params[:requestee_user_id])
       requester = User.find(current_user.id)
-      requestee.confirm_friend(requester)
-      # f = Friendship.find_by(user_id: requestee.id, friend_id: requester.id)
-      # f.status = true
-      # f.save
+      # requestee.confirm_friend(requester)
+      f = Friendship.find_by(user_id: requestee.id, friend_id: requester.id)
+      f.status = true
+      f.save
       redirect_to user_friend_requests_path, notice: 'Friend confirmed'
     else
       redirect_to user_friend_requests_path, alert: 'Something went wrong'
@@ -29,7 +29,7 @@ class FriendshipsController < ApplicationController
                                      friend_id: params[:requestee_user_id])
     if @friendship
       @friendship.destroy
-      redirect_to user_friend_requests_path, notice: 'You have deleted user from your friends'
+      redirect_to user_friend_requests_path, notice: 'Friend removed'
     else
       redirect_to root_path, alert: 'You are not allowed to do this'
     end
