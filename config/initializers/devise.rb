3d463b3a377e7ba0b7585862556390a6d88235c9
J. Christian Romero <jcromerohdz@gmail.com>
2019-12-26 12:22:24 -0800

    Adding facebook login functionality

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,4 +296,7 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  # Facebook credentials
+    config.omniauth :facebook, 'app_id', 'secret_key', callback_url: "http://localhost:3000/auth/facebook/callback"
 end
